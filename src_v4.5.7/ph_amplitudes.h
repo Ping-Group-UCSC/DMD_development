e47bb62b24c90e9c6306036f59721a4041ce1d24
Jacopo Simoni <jsimoni@wisc.edu>
2025-06-16 15:44:16 -0500

    create ph_amplitudes class

--- src_v4.5.7/ph_amplitudes.h
+++ src_v4.5.7/ph_amplitudes.h
@@ -1,23 +1,32 @@
 #pragma once
 #include "common_headers.h"
+#include "phonon.h"
 
 class ph_amplitudes {
 public:
-    lattice *latt;
+    int modeStart, modeEnd;
+    std::vector<complex> Bq;
 
 
 
 
-
-    ph_amplitudes (lattice *latt, parameters *param): latt(latt)
+    ph_amplitudes (parameters *param, phonon *ph): modeStart(param->modeStart), modeEnd(param->modeEnd)
     {
-        if (ionode) std::cout << "PH. AMPLITUDES INITIALIZATION" << std::endl;
+        // OK
+        init (ph);
     }
 
 
+    void init (phonon *ph) {
 
+        if (ionode) std::cout << "PH. AMPLITUDES INITIALIZATION" << std::endl;
+        if (ionode) std::cout << "N. MODES: " << ph->nm << std::endl;
+        if (ionode) std::cout << "MODES ST. -> END: " << modeStart << " - " << modeEnd << std::endl;
 
+        alloc_Bq_arrays ();
+    }
 
+    void alloc_Bq_arrays ();
 
 
 
