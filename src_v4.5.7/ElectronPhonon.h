12f939e9d9d9a3bf3433811a3e7ea3b3406e2662
Jacopo Simoni <jsimoni@wisc.edu>
2025-11-11 08:32:33 -0600

    define Coulomb_model_base class and declare coul_model as 2D or 3D

--- src_v4.5.7/ElectronPhonon.h
+++ src_v4.5.7/ElectronPhonon.h
@@ -26,8 +26,7 @@ public:
 	elecelec_model *ee_model;
 	const double t0, tend, degauss;
 	const double prefac_gaussexp, prefac_sqrtgaussexp;
-	coulomb_model *coul_model;
-	coulomb_model_base *screen_model;
+	coulomb_model_base *coul_model;
 
 	double prefac_gauss, prefac_sqrtgauss, prefac_eph, scale_scatt, scale_eph, scale_ei, scale_ee;
 	int nk_glob, nk_proc, ik0_glob, ik1_glob;
@@ -90,16 +89,13 @@ public:
 			// define object based on the condition
 			int DimValue = latt->dim;
 			if (DimValue == 2) {
-				screen_model = new coulomb_model_2D(latt, param, elec, bStart, bEnd, eEnd-eStart);
+				coul_model = new coulomb_model_2D(latt, param, elec, bStart, bEnd, eEnd-eStart);
 			}
 			else if (DimValue == 3) {
-				screen_model = new coulomb_model_3D(latt, param, elec, bStart, bEnd, eEnd-eStart);
+				coul_model = new coulomb_model_3D(latt, param, elec, bStart, bEnd, eEnd-eStart);
 			}
 		}
-		if (ionode) std::cout << "OK UP TO HERE" << std::endl;
-		if (ionode) screen_model->print();
-		exit(1);
-		coul_model = new coulomb_model(latt, param, elec, bStart, bEnd, eEnd - eStart);
+		
 		if (eip.ni.size() > 0) eimp = new electronimpurity*[eip.ni.size()]{nullptr};
 		for (int iD = 0; iD < eip.ni.size(); iD++){
 			eimp[iD] = new electronimpurity(iD, mp, isHole, nkpair_glob, nb, latt->cell_size);
